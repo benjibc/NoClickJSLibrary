80645de145fb274cf122bb5554be979ddf9070c7
Benny(Yufei) Chen <youfychenbc5000@gmail.com>
2012-05-22 00:01:02 -0700

    added manual callback and better demo

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
 NoClickJSLibrary
 ================
-The intention of this library is turning any website into a website that
+The intention of this library is turning any website into a something that
 requires no clicking to go around. This will be a small project of my own that
 will not be updated very frequently.
 
-The library is under development right now, therefore please be patient before
-you file any complaints about the speed of the development.
+The library only has very basic circling feature at the moment.
+
+In order to use the library, add a few lines into your code.
+<script type="text/javascript">
+$(document).ready(function(){
+var mousetracker1 = new MouseTracker();
+mousetracker1.addElement("test_area");
+});
+</script>
+
+MouseTracker class is the class that deals with all of the tracking and signal.
+By default, MouseTracker will not be active unless you use its member function
+addElement to add specifically an element to be tracked by putting down the
+ID of the element's .
+
+You can add a div into your html called "debug_tracker" to see the position of
+the mouse
+
+Note: when you add the element, ALL of its children will also be tracked. 
+By default, all the buttons circled will be triggered with a 'click' action,
+and all of the input field circled will be triggered with a 'enter' action.
 
 Please file any bug report at Github, so people can deal with it collectively.
 
--- js/noclick.js
+++ js/noclick.js
@@ -9,20 +9,20 @@
 //To increase the efficiency, we will not be calculating the distance using
 //sqrt here. we will be calculating the
 
-$(document).ready(function() {
 function MouseTracker ()
 {
   ElementList = new Array();
+  //master list that keeps all the item in an array
   this.m_ElementList = ElementList;
-  //simply loop through array to ensure maximum performance
-  this.global_x = 0;
-  this.global_y = 0;
   this.track();
 }
 
-MouseTracker.prototype.addElement = function(contentDivID){
-  var Element_ID = 0;
-  var content = $("#"+contentDivID).html();
+MouseTracker.prototype.addElement = function(contentDivID, callback){
+  callback = typeof callback !== 'undefined' ? callback : null;
+  var Element_ID = ElementList.length;
+  ElementList.push(new Element( $("#"+contentDivID), Element_ID, 
+  contentDivID, callback));
+  Element_ID++;  
   $("#"+contentDivID).find('*').each(function (){
     var kid = $(this);
     ElementList.push(new  Element(kid, Element_ID,
@@ -36,53 +36,142 @@ MouseTracker.prototype.track = function()
 {
    $(document).mousemove(function(e){
       $("#debug_tracker").text(e.pageX +', '+ e.pageY);
-      for(var i = 0; i < ElementList.length; i++)
-      { 
+      for(var i = 0; i < ElementList.length; i++){ 
         ElementList[i].activate(e.pageX, e.pageY);
       }
    }); 
 }
 
-MouseTracker.prototype.getElementCoord= function (ElementId)
+function Element( curObject, ID, htmlID, callback)
 {
-  var position = $("#".ElementId).position();
-  return curCoord;
+  this.m_ID = ID;
+  this.htmlID = htmlID;
+  this.object = curObject;
+  this.type = curObject[0].nodeName;
+  position = curObject.position();
+  this.width = curObject.outerWidth();
+  this.height = curObject.outerHeight();
+  this.y_top = position.top;
+  this.y_bottom = position.top + this.height;
+  this.y_center = position.top + (this.height /2);
+  this.x_left = position.left;
+  this.x_right = position.left + this.width;
+  this.x_center = position.left + (this.width /2);
+  this.activationStage = 0;
+  this.prevPos = 0;
+  if(callback == null)
+  this.initialise();
+  else
+    this.callback = callback;
+  this.tolerance= 8;
+}
+Element.prototype.trigger = function ()
+{
+  this.callback();
+}
+//counter clockwise orientation. 1 is top, 2 is left, 3 is bottom, 4 is right
+Element.prototype.activate = function (x,y)
+{
+  //console.log(this.activationPosition(x,y));
+  var curPos = this.activationPosition(x,y);
+  switch(curPos)
+  {
+     case 0:
+       break;
+     case 1:  //activated. only 0(none) 2(left) and 4(right) is allowed
+     case 3:  //activated. only 0(none) 2(left and 4(right) is allowed
+  console.log(this.activationStage + " " + this.prevPos);
+       if(this.prevPos == 0){
+         this.activationStage++;
+         this.prevPos = curPos
+         break;
+       }
+       if(this.prevPos==2||this.prevPos==4){
+         this.prevPos = curPos
+         this.activationStage++;
+       }
+       break;
+     case 2:  //activated. only 1(top) and 3(down) is allowed
+     case 4:  //activated. only 1(top) and 3(down) is allowed
+  console.log(this.activationStage + " " + this.prevPos);
+       if(this.prevPos == 0){
+         this.prevPos = curPos
+         this.activationStage++;
+         break;
+       }
+       if(this.prevPos % 2 == 1){
+         this.prevPos = curPos
+         this.activationStage++; 
+       }
+       break;
+     default: //error. set to zero
+       console.log("Element activation error");
+  }
+
+  if(this.activationStage == 4)
+  {
+    this.callback();
+    this.activationStage = 0;
+  }
 }
 
-var mouseTracker1 = new MouseTracker();
-mouseTracker1.addElement("test_area");
-});
+//counter clockwise orientation. 1 is top, 2 is left, 3 is bottom, 4 is right
+Element.prototype.activationPosition = function(x,y)
+{
+  if((absVal(y - this.y_top)<this.tolerance) && 
+     (absVal(x - this.x_center) < this.tolerance)){
+     return 1; 
+  }
+  else if(absVal(y - this.y_bottom )< this.tolerance && 
+          absVal(x - this.x_center) < this.tolerance){
+    return 3;    
+  }
+  
+  if(absVal(x-this.x_right) < this.tolerance 
+     && absVal(y - this.y_center) < this.tolerance){
+      return 4; 
+  }
+  else if(absVal(x-this.x_left) < this.tolerance && 
+          absVal(y - this.y_center) < this.tolerance){
+    return 2;
+  }
+  //nowhere close to any element
+  return 0;
+}
 
-        /*if(absVal(e.pageX - ElementList[i].x_center) < 5)
-        {
-          if(absVal(e.pageY - ElementList[i].y_top) < 5 ){  
-             //console.log("came close top!"); 
-             ElementList[i].activationStage ++;
-             ElementList[i].activate(0);
-          } 
-          if(absVal(e.pageY - ElementList[i].y_bottom) < 5 ){
-             //console.log("came close buttom!"); 
-             ElementList[i].activationStage ++;
-             ElementList[i].activate(2);
-          }
-        }
+Element.prototype.callback = function()
+{
+  alert("Callback not yet implemented");
+} 
 
-        if(absVal(e.pageY -ElementList[i].y_center) <5)
-        {
-          if(absVal(e.pageX - ElementList[i].x_left) < 5) {
-             //console.log("came close left!"); 
-             ElementList[i].activationStage ++;
-             ElementList[i].activate(1);
-          }
-          if(absVal(e.pageX - ElementList[i].x_right) < 5) {
-             //console.log("came close right!"); 
-             ElementList[i].activationStage ++;
-             ElementList[i].activate(3);
-          }
-        }
+Element.prototype.initialise = function()
+{
+  if(this.type == "P" || this.type == "DIV")
+    this.callback = function(){};
+  if(this.type == "INPUT")
+  {
+    var inputType = this.object.attr('type');
+    if(inputType == 'password')
+      this.callback = function()
+      {
+        this.object.trigger('enter');
+      };
+    else if(inputType == 'submit')
+      this.callback = function()
+      {
+        this.object.trigger('submit');
+      }
+    else
+      this.callback = function()
+      {
+        this.object.trigger('click');
+        return;
+      }
+  }
+}
 
-        if(ElementList[i].activationStage == 4){
-          console.log("activated");
-          ElementList[i].activationStage = 0;
-          ElementList[i].callback();
-        }*/
+function absVal(val)
+{
+  return Math.abs(val);
+}
+
